f0420c2a74d52c1cfc25910068f8a44dd081b08f
sae22246323 <66347024+sae22246323@users.noreply.github.com>
2022-04-27 15:23:11 +0800

    Update User.java

--- springboot/src/main/java/com/example/demo/entity/User.java
+++ springboot/src/main/java/com/example/demo/entity/User.java
@@ -22,38 +22,6 @@ public class User {
     private String password;
     //private String token;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Date getBirthday() {
-        return birthday;
-    }
-
-    public void setBirthday(Date birthday) {
-        this.birthday = birthday;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-    
 
 
     @Override
